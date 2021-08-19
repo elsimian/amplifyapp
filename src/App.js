f2940f0819f973e34ac05ed89216cca071dc16b0
Simon Russell <simonru@amazon.co.uk>
2021-08-19 13:52:49 +0100

    added url for testing

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have Authentication!</h1>
       </header>
+      <a href="https://7yjxg3ieim.labeling.eu-west-1.sagemaker.aws">Labelling Place</a>
       <AmplifySignOut />
     </div>
   );
